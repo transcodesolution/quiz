13745b8cdd024ea2f127b1c867db94f85320df82
Kunjesh <transcode.kunjesh@gmail.com>
2025-02-06 16:38:13 +0530

    pop fixes

--- adscripts/quizTcz_Home.js
+++ adscripts/quizTcz_Home.js
@@ -1,6 +1,15 @@
-window.googletag = window.googletag || {cmd: []};
-googletag.cmd.push(function() {
-  googletag.defineSlot('/23201071713/quizTcz_quizeHome', [[336, 280], [480, 320], [300, 250]], 'div-gpt-ad-1738058472013-0').addService(googletag.pubads());
+window.googletag = window.googletag || { cmd: [] };
+googletag.cmd.push(function () {
+  googletag
+    .defineSlot(
+      "/23201071713/quizTcz_quizeHome",
+      [
+        [336, 280],
+        [300, 250],
+      ],
+      "div-gpt-ad-1738058472013-0"
+    )
+    .addService(googletag.pubads());
   googletag.pubads().collapseEmptyDivs();
   googletag.enableServices();
 });
