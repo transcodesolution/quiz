31ad6f83918ee4f6c0f489dde8f802a556ed1a45
Master <master.transcode@gmail.com>
2026-08-27 10:43:37 +0530

    larapush code added

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+const options = {
+    firebaseConfig: {
+        projectId: "min-lpush",
+        messagingSenderId: "1087775886296",
+        appId: "1:1087775886296:web:824d1ce05f6ed5639cc0ad",
+        apiKey: "AIzaSyCPOG6kuUoeBk-KedMPnPECoyljJVoWf3I",
+    },
+    domain: "fiveminutesgames.com",
+    api_url: "https://push.fiveminutesgames.com/api/token",
+    vapid_public_key: "BPZ1_UmeYX6y7TIkwmAiqtK6-qCVfcbkZw02dH3HMCs-_uW8sjGOqFO6U3Q-b346lFp7DPqwvPlnt7O7d2_lP5Y",
+    http: 0,
+    one_time_collect: 1,
+}
+
+importScripts("https://cdn.larapush.com/sw/larapush-sw-v5.min.js");
